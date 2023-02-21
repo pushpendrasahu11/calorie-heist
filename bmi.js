bd268109eefc8824f96096eb000fff59b0b546a7
Pushpendra <pushpendrasahu1122@gmail.com>
2023-02-21 19:54:00 +0530

    navbar updated 1

--- bmi.js
+++ bmi.js
@@ -10,3 +10,53 @@ ham.addEventListener('click',() => {
 close.addEventListener('click',() => {
     nav.classList.remove('active');
 })
+
+
+
+
+function getbmi(w,h){
+let hunit= document.getElementById('selecth');
+let wtunit= document.getElementById('selectw');
+
+let hunitis= hunit.options[hunit.selectedIndex].text;
+let wtunitis= wtunit.options[wtunit.selectedIndex].text;
+
+if(hunitis==='feet') {
+    h=h*30.48;
+}
+if(wtunitis === 'pounds'){
+    w=w*0.453592;
+}
+
+return w / (h*h);
+}
+
+	
+// BMI Categories:
+// Underweight = <18.5
+// Normal weight = 18.5–24.9
+// Overweight = 25–29.9
+// Obesity = BMI of 30 or greater
+
+function calculateBmi(){
+    let h= document.getElementById("height").value;
+    let w= document.getElementById("weight").value;
+    
+    let result = document.getElementById('ans');
+    let bmi = 10000* getbmi(w,h);
+
+    let status = document.getElementById('status');
+
+    result.innerText=bmi.toPrecision(3);
+    
+
+    if(bmi<= 18.5) {
+        status.innerText = "You are Underweight !";
+    }else if(bmi<=24.9) status.innerText = "You have Normal Weight";
+    else if(bmi<=29.9) status.innerText = "You are Overweight !";
+    else if(bmi>=30)status.innerText = "You have Obesity !!";
+    else status.innerText = "Fill the fields correctly";
+
+    
+}
+
--- navbar.js
+++ navbar.js
@@ -10,11 +10,3 @@ ham.addEventListener('click',() => {
 close.addEventListener('click',() => {
     nav.classList.remove('active');
 })
-
-// let c= document.getElementById('change');
-
-// if(c.checked){
-//     console.log("Now this is Sign up / register mode ");
-// } else{
-//     console.log("Now this is Sign in mode ");
-// }
