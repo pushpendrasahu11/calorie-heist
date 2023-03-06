9be7aac77b5fbc9da99e0509831972b748cd7f22
Pushpendra <pushpendrasahu1122@gmail.com>
2023-03-06 19:37:14 +0530

    file arranged

--- js/bmi.js
+++ js/bmi.js
@@ -1,19 +1,3 @@
-// for navbar
-let ham = document.getElementById('ham');
-let nav = document.getElementById('navbar');
-let close = document.getElementById('close')
-
-ham.addEventListener('click',() => {
-        nav.classList.add('active');
-})
-
-close.addEventListener('click',() => {
-    nav.classList.remove('active');
-})
-
-
-
-
 function getbmi(w,h){
 let hunit= document.getElementById('selecth');
 let wtunit= document.getElementById('selectw');
